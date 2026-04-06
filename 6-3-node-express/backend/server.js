e8580cfac1de06ad4eb74cf054b5fc0d82e914cf
leen7777 <s202359170@kfupm.edu.sa>
2026-04-06 13:25:37 +0300

    server running on port 3000

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -2,7 +2,6 @@
 
 // TODO 1: Define server port
 
-
 // TODO 4: Import and Apply CORS middleware
 
 
@@ -16,3 +15,26 @@
 
 
 // TODO 7: Start server using app.listen
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { getRandomQuote } from "./quotes.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
